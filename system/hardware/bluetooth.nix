224861f1ba253323a6b965dc31d95b86adff75cd
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2024-03-30 17:39:25 +0530

    Kernel bluetooth experimental features

--- system/hardware/bluetooth.nix
+++ system/hardware/bluetooth.nix
@@ -4,7 +4,10 @@
   hardware.bluetooth = {
     enable = true; # enables support for Bluetooth
     powerOnBoot = true; # powers up the default Bluetooth controller on boot
-    settings.General.Experimental = true; # enable bluetooth battery percentage features
+    settings.General = {
+      Experimental = true; # enable bluetooth battery percentage features
+      KernelExperimental = true;
+    };
   };
   services.blueman.enable = true; # enables the Bluetooth manager
 }
