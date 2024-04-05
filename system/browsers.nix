22bdb33b317d751b028e4494296e130dd8b8d963
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2024-04-05 11:28:51 +0530

    New chrome flags added

--- system/browsers.nix
+++ system/browsers.nix
@@ -18,20 +18,39 @@
       # change it if you have any issues
       # note the spaces, they are required
       # Vulkan is not stable, likely because of bad drivers
+      # Flags enabled by command line have no need to be enabled in chrome://flags
       commandLineArgs = ""
         + " --enable-accelerated-video-decode"
+        + " --enable-accelerated-vpx-decode"
         + " --enable-accelerated-mjpeg-decode"
         + " --enable-gpu-compositing"
-        + " --enable-gpu-rasterization" # dont enable in about:flags
+        + " --enable-gpu-rasterization"
         + " --enable-native-gpu-memory-buffers"
         + " --enable-raw-draw"
-        + " --enable-zero-copy" # dont enable in about:flags
-        + " --ignore-gpu-blocklist" # dont enable in about:flags
+        + " --enable-zero-copy"
+        + " --ignore-gpu-blocklist"
         # + " --use-vulkan"
         + " --enable-features="
-          + "VaapiVideoEncoder,"
-          + "CanvasOopRasterization,"
-          # + "Vulkan"
+          + "ParallelDownloading" # Faster downloads
+          + ",VaapiVideoEncoder" # Video encoding support
+          + ",CanvasOopRasterization"
+          + ",UseDMSAAForTiles"
+          + ",UseGpuSchedulerDfs"
+          + ",UIEnableSharedImageCacheForGpu" # Shared image cache
+          + ",UseClientGmbInterface" # new ClientGmb interface to create GpuMemoryBuffers
+          # + ",SkiaGraphite"
+          # + ",EnableDrDc"
+          # + ",Vulkan"
+          # + ",VulkanFromANGLE"
+          + ",PostQuantumKyber" # hybrid kyber for enhanced TLS security
+          + ",PulseaudioLoopbackForCast" # Audio support for casting and screen sharing
+          + ",PulseaudioLoopbackForScreenShare"
+          + ",ChromeWideEchoCancellation" # noise cancellation for WebRTC
+          + ",DesktopScreenshots"
+          + ",FluentOverlayScrollbar" # New scrollbar
+          + ",FluentScrollbar"
+          + ",EnableTabMuting" # Mute tabs from tab context
+          + ",GlobalMediaControlsUpdatedUI" # New media controls, with PIP
         ;
     })
   ];
