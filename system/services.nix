167d7ce547661ddcf59ab80d27a922b42f05cfbd
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2024-04-17 10:05:09 +0530

    Add patches of ananicy-cpp-rules from upstream

--- system/services.nix
+++ system/services.nix
@@ -1,4 +1,4 @@
-# Configure printers
+# Configure system services
 { pkgs, ... }:
 
 {
@@ -31,7 +31,23 @@
   # Enable Ananicy CPP for better system performance
   services.ananicy = {
     enable = true;
-    rulesProvider = pkgs.ananicy-cpp-rules; # from nixpkgs: ananicy-rules-cachyos
+    # from nixpkgs: ananicy-rules-cachyos
+    rulesProvider = pkgs.ananicy-cpp-rules.overrideAttrs (oldAttrs: rec {
+      patches = [
+        (pkgs.fetchpatch {
+          # FIXME: remove when https://github.com/CachyOS/ananicy-rules/pull/80 is merged and available in nixpkgs
+          name = "add-compiler-rules.patch";
+          url = "https://patch-diff.githubusercontent.com/raw/CachyOS/ananicy-rules/pull/80.diff";
+          hash = "sha256-GF2bjOaCkNaAP160C7Cs3DYs2FId5vcKeErG0ToHRbA=";
+        })
+        (pkgs.fetchpatch {
+          # FIXME: remove when https://github.com/CachyOS/ananicy-rules/pull/84 is merged and available in nixpkgs
+          name = "add-xdg-gvfs-misc.patch";
+          url = "https://patch-diff.githubusercontent.com/raw/CachyOS/ananicy-rules/pull/84.patch";
+          hash = "sha256-S2m+/xgjuAhFg7Ta4X5z150xgYEl917lw8wtoWU3C3M=";
+        })
+      ];
+    });
   };
 
   # Some programs need SUID wrappers, can be configured further or are
