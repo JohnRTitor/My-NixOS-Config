2475d3962d1a7b664dbfcfae575cec00a90ba480
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2024-04-29 18:26:01 +0530

    Add ananicy-services patch

--- system/services/ananicy-cpp.nix
+++ system/services/ananicy-cpp.nix
@@ -4,6 +4,15 @@
   services.ananicy = {
     enable = true;
     # from nixpkgs: ananicy-rules-cachyos
-    rulesProvider = pkgs.ananicy-cpp-rules;
+    rulesProvider = pkgs.ananicy-cpp-rules.overrideAttrs (oldAttrs: {
+      patches = [
+        (pkgs.fetchpatch {
+          # FIXME: remove when https://github.com/CachyOS/ananicy-rules/pull/87 is merged and available in nixpkgs/chaotic
+          name = "update-services-rules.patch";
+          url = "https://patch-diff.githubusercontent.com/raw/CachyOS/ananicy-rules/pull/87.patch";
+          hash = "sha256-Xu9Kd9YJKEamaHLqCi/b0bl7pdjwO/OzCCEr7cX3VuY=";
+        })
+      ];
+    });
   };
 }
