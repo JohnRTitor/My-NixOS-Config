c36a080edb5c088dbe9812cc063ad316518518c2
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2024-08-10 13:36:25 +0530

    disks: removed swap

--- system/hardware/disk.nix
+++ system/hardware/disk.nix
@@ -23,6 +23,7 @@
     memoryPercent = 200;
   };
 
+  /* SWAP DELETED
   swapDevices = [
     {
       device = "/dev/disk/by-partuuid/90c8cb42-7424-467c-927a-0d6a63d5b2a2";
@@ -36,6 +37,7 @@
       };
     } # 16 Gigs swap
   ];
+  */
 
   boot.kernel.sysctl = {
     # Setting High swappiness can improve performance with zram
